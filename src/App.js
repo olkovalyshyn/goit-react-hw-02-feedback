a812c5f450115caa6c55866e4d4371a3eef3a013
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-12-16 22:21:25 +0200

    Add HW-02 (Task-01 done)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import Feedback from "./components/Feedback/Feedback";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Feedback />
-    </div>
-  );
+import Statistics from "./components/Statistics/Statistics";
+import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import Section from "./components/Section/Section";
+import Notification from "./components/Notification/Notification";
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  clickEventBtn = (option) => {
+    this.setState((state) => {
+      return { [option]: state[option] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return (this.state.good / this.countTotalFeedback()) * 100;
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={["good", "neutral", "bad"]}
+            onLeaveFeedback={this.clickEventBtn}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={Math.round(
+                this.countPositiveFeedbackPercentage()
+              )}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,9 +1,10 @@
 import React from "react";
+import s from "./Section.module.css";
 
 export default function Section({ title, children }) {
   return (
     <div>
-      <h2>{title}</h2>
+      <h2 className={s.title}>{title}</h2>
       {children}
     </div>
   );
